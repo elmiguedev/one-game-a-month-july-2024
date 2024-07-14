abaf84cfe042248ef2714a0502f6274730c35425
migue <mmoyano16@gmail.com>
2024-07-14 11:51:09 -0300

    feat: add sounds

---
+++ src/entities/raids/PlanningRaid.ts
@@ -0,0 +1,48 @@
+import { Scene } from "phaser";
+import { TrailObstacles } from "../obstacles/TrailObstacles";
+import { AlternateTrailObstacles } from "../obstacles/AlternateTrailObstacles";
+import { GameHud } from "../GameHud";
+
+/**
+ * A raid sent on a daily metting 
+ */
+
+const RAID_TIME = 4000;
+
+export class PlanningRaid {
+  constructor(
+    private scene: Scene,
+    private obstacles: Phaser.Physics.Arcade.Group,
+    private velocity: number,
+    private gameHud: GameHud
+  ) {
+    this.setMeetingMode();
+    console.log("crea daily raid")
+    let i = 0;
+    new TrailObstacles(scene, obstacles, "jira", velocity);
+    scene.time.addEvent({
+      delay: RAID_TIME,
+      repeat: 1,
+      callback: () => {
+        switch (++i) {
+          case 1:
+            new AlternateTrailObstacles(scene, obstacles, "chat", velocity);
+            break;
+          case 2:
+            new TrailObstacles(scene, obstacles, "changuito", velocity);
+          default:
+            break;
+        }
+      }
+    })
+  }
+
+  private setMeetingMode() {
+    this.gameHud.setMeetingMode("Daily");
+    this.scene.cameras.main.setBackgroundColor(0x94baff);
+    this.scene.time.delayedCall(12000, () => {
+      this.gameHud.endMeeting();
+      this.scene.cameras.main.setBackgroundColor(0xffffff);
+    })
+  }
+}
