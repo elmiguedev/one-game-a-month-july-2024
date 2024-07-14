5e5200fbdafec302879fe3917b8d5e071063867d
migue <mmoyano16@gmail.com>
2024-07-14 11:30:55 -0300

    feat: add boss

--- src/entities/raids/BossRaid.ts
+++ src/entities/raids/BossRaid.ts
@@ -9,23 +9,47 @@ export class BossRaid {
 
   constructor(scene: Scene, obstacles: Phaser.Physics.Arcade.Group, velocity: number) {
     const x = scene.game.canvas.width - 240;
+    const initialX = x + 500;
     const y = scene.game.canvas.height - 450;
-    const boss = new BossObstacle(scene, x, y);
+    const boss = new BossObstacle(scene, initialX, y);
     obstacles.add(boss);
 
-    new TrailObstacles(scene, obstacles, "octocat", velocity);
+    scene.add.tween({
+      targets: boss,
+      x: x,
+      duration: 4000,
+      ease: "Power1",
+      onComplete: () => {
+        new TrailObstacles(scene, obstacles, "octocat", velocity);
 
-    scene.time.delayedCall(4000, () => {
-      new AlternateTrailObstacles(scene, obstacles, Obstacle.getRandomObstacleType(), velocity);
-    });
+        scene.time.delayedCall(4000, () => {
+          new AlternateTrailObstacles(scene, obstacles, Obstacle.getRandomObstacleType(), velocity);
+        });
 
-    scene.time.delayedCall(8000, () => {
-      new AlternateZigZagObstacles(scene, obstacles, Obstacle.getRandomObstacleType(), velocity);
-    });
+        scene.time.delayedCall(8000, () => {
+          new AlternateZigZagObstacles(scene, obstacles, Obstacle.getRandomObstacleType(), velocity);
+        });
 
-    scene.time.delayedCall(12000, () => {
-      new TrailObstacles(scene, obstacles, "changuito", velocity);
-    });
+        scene.time.delayedCall(12000, () => {
+          new TrailObstacles(scene, obstacles, "changuito", velocity);
+        });
+
+        scene.time.delayedCall(15000, () => {
+          scene.add.tween({
+            targets: boss,
+            x: x + 500,
+            duration: 4000,
+            ease: "Power1",
+            onComplete: () => {
+              boss.destroy();
+            }
+          })
+        });
+      }
+    })
+
+
+
 
 
   }
